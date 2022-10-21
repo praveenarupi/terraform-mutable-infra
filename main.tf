0a804f1354482c3182ec6801085f00ffc9210ffa
praveenarupi <praveenarupi@gmail.com>
2022-10-21 15:37:00 +0530

    updating ./terraform-mutable-infra

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "docdb" {
   source                 = "./vendor/modules/docdb/"
   env                    = var.env
   engine_version         = var.docdb_engine_version
-  apps_subnets_ids = local.apps_subnets_ids
+  db_subnets_ids = local.db_subnets_ids
   vpc_id = module.vpc.vpc_id
   vpc_cidr_block = var.vpc_cidr_block
   instance_count = var.docdb_cluster_instances_count
